cccf4e34bac1e2755f532b00f467b1d14aa36171
pikerar <dshelushko@gmail.com>
2025-12-05 13:40:18 +0700

    Почему тут насрано файлами

--- BookLibrary.Core/BookModel.cs
+++ BookLibrary.Core/BookModel.cs
@@ -7,6 +7,22 @@ using System.Collections.Generic;
 
 namespace BookLibrary.Core
 {
+    public interface IBookModel
+    {
+        IEnumerable<Book> GetBooksPage(int page, int pageSize, out int totalBooks);
+        Book? GetBook(int id);
+
+        void CreateBook(string title, string author, int year, int genreId,
+                        out string message, out bool success);
+
+        void UpdateBook(int id, string title, string author, int year, int genreId,
+                        out string message, out bool success);
+
+        void DeleteBook(int id, out string message, out bool success);
+
+        IEnumerable<Genre> GetAvailableGenres();
+    }
+
     public class BookModel : IBookModel
     {
         private readonly BookLogic _logic;
